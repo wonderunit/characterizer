e7febfa4804c98c797d89e9a090c98febf3dc4df
Robby Abaya <rabaya@gmail.com>
2017-10-13 14:28:25 -0400

    Only fetch the character values for the view that needs it in conflict comparison view.

--- js/window/character-comparison-conflict-view.js
+++ js/window/character-comparison-conflict-view.js
@@ -30,100 +30,102 @@ module.exports = class CharacterComparisonConflictView extends CharacterComparis
   }
 
   updateView() {
+    this.comparisonView.innerHTML = ``
+    let headersContainer = document.createElement("div")
+    headersContainer.classList.add("comparison-view-conflicts-header-container")
+    this.comparisonView.appendChild(headersContainer)
+
+    let container = document.createElement("div")
+    container.classList.add("comparison-view-conflicts-values-container")
+    for(let i = 0; i<this.selectedCharacters.length; i++) {
+      if(i > 0) {
+        let vsView = document.createElement("div")
+        vsView.innerHTML = `vs`
+        headersContainer.appendChild(vsView)
+      }
+      let characterName = document.createElement("h2")
+      characterName.classList.add("comparison-view-conflicts-header")
+      characterName.innerHTML = this.selectedCharacters[i].name
+      headersContainer.appendChild(characterName)
+    }
+
+    let conflictContainer
+    switch(this.viewType) {
+      case "favorites, paired":
+        conflictContainer = this.getFavoriteValuesView() 
+        break
+      case "favorited pairs":
+        conflictContainer = this.getFavoritePairedValuesView()
+        break
+      case "all":
+      default:
+        conflictContainer = this.getValuesView()
+        break
+    }
+    this.comparisonView.appendChild(conflictContainer)
+  }
+
+  getValuesView() {
+    let result = document.createElement("div")
+
     this.getSelectedCharacterValues()
-    .then(inCharacterValueResults => {
+      .then(inCharacterValueResults => {
         let characterValueResults = []
         for(let i = 0; i < inCharacterValueResults.length; i++) {
           let values = inCharacterValueResults[i].slice(0, NUM_COMPARISON_ITEMS)
           characterValueResults.push(semiRandomShuffle(values, RANDOM_SHUFFLE_FACTOR))
         }
 
-        this.comparisonView.innerHTML = ``
-        let headersContainer = document.createElement("div")
-        headersContainer.classList.add("comparison-view-conflicts-header-container")
-        this.comparisonView.appendChild(headersContainer)
-
-        let container = document.createElement("div")
-        container.classList.add("comparison-view-conflicts-values-container")
-        for(let i = 0; i<this.selectedCharacters.length; i++) {
-          if(i > 0) {
-            let vsView = document.createElement("div")
-            vsView.innerHTML = `vs`
-            headersContainer.appendChild(vsView)
+        for(let valueIndex = 0; valueIndex < NUM_COMPARISON_ITEMS; valueIndex++) {
+          let conflictContainer = document.createElement("div")
+          conflictContainer.classList.add("comparison-view-conflict-container")
+      
+          let favButton = document.createElement('div')
+          favButton.innerHTML = `add favorite`
+          conflictContainer.appendChild(favButton)
+      
+          // we use this in the add favorite event.
+          let favoriteData = {}
+          // we use this to check if the favorite pair exists.
+          let favoritesPaths = []
+          for(var j = 0; j < characterValueResults.length; j++) {
+            if(j > 0) {
+              let vsView = document.createElement("div")
+              vsView.innerHTML = `vs`
+              conflictContainer.appendChild(vsView)
+            }
+            let characterValues = characterValueResults[j]
+            let value = characterValues[valueIndex]
+            let name = this.valuesMap[value.valueID].name
+            let nameView = document.createElement("div")
+            nameView.innerHTML = name
+            conflictContainer.appendChild(nameView)
+      
+            favoriteData[`character${j+1}ID`] = value.characterID
+            favoriteData[`value${j+1}ID`] = value.valueID
+            favoritesPaths.push([value.characterID, value.valueID])
           }
-          let characterName = document.createElement("h2")
-          characterName.classList.add("comparison-view-conflicts-header")
-          characterName.innerHTML = this.selectedCharacters[i].name
-          headersContainer.appendChild(characterName)
+      
+          let isFavorite = false
+          if(favoritesPaths.length > 1) {
+            isFavorite = utils.checkObjectPath(favoritesPaths[0].concat(favoritesPaths[1]), this.valueComparisonFavorites) 
+              || utils.checkObjectPath(favoritesPaths[1].concat(favoritesPaths[0]), this.valueComparisonFavorites)
+          }
+          if(isFavorite) {
+            favButton.innerHTML = `favorited`
+          } else {
+            var self = this
+            favButton.addEventListener('mouseup', function(event) {
+              event.target.innerHTML = `favorited`
+              self.emit('add-comparison-favorite', favoriteData)
+            })
+          }
+    
+          result.appendChild(conflictContainer)
         }
-
-        let conflictContainer
-        switch(this.viewType) {
-          case "favorites, paired":
-            conflictContainer = this.getFavoriteValuesView() 
-            break
-          case "favorited pairs":
-            conflictContainer = this.getFavoritePairedValuesView()
-            break
-          case "all":
-          default:
-            conflictContainer = this.getValuesView(characterValueResults)
-            break
-        }
-        this.comparisonView.appendChild(conflictContainer)
       })
       .catch(console.error)
-  }
-
-  getValuesView(characterValueResults) {
-    let result = document.createElement("div")
-    for(let valueIndex = 0; valueIndex < NUM_COMPARISON_ITEMS; valueIndex++) {
-      let conflictContainer = document.createElement("div")
-      conflictContainer.classList.add("comparison-view-conflict-container")
-  
-      let favButton = document.createElement('div')
-      favButton.innerHTML = `add favorite`
-      conflictContainer.appendChild(favButton)
-  
-      // we use this in the add favorite event.
-      let favoriteData = {}
-      // we use this to check if the favorite pair exists.
-      let favoritesPaths = []
-      for(var j = 0; j < characterValueResults.length; j++) {
-        if(j > 0) {
-          let vsView = document.createElement("div")
-          vsView.innerHTML = `vs`
-          conflictContainer.appendChild(vsView)
-        }
-        let characterValues = characterValueResults[j]
-        let value = characterValues[valueIndex]
-        let name = this.valuesMap[value.valueID].name
-        let nameView = document.createElement("div")
-        nameView.innerHTML = name
-        conflictContainer.appendChild(nameView)
-  
-        favoriteData[`character${j+1}ID`] = value.characterID
-        favoriteData[`value${j+1}ID`] = value.valueID
-        favoritesPaths.push([value.characterID, value.valueID])
-      }
-  
-      let isFavorite = false
-      if(favoritesPaths.length > 1) {
-        isFavorite = utils.checkObjectPath(favoritesPaths[0].concat(favoritesPaths[1]), this.valueComparisonFavorites) 
-          || utils.checkObjectPath(favoritesPaths[1].concat(favoritesPaths[0]), this.valueComparisonFavorites)
-      }
-      if(isFavorite) {
-        favButton.innerHTML = `favorited`
-      } else {
-        var self = this
-        favButton.addEventListener('mouseup', function(event) {
-          event.target.innerHTML = `favorited`
-          self.emit('add-comparison-favorite', favoriteData)
-        })
-      }
-
-      result.appendChild(conflictContainer)
-    }
+    
     return result
   }
   
